0925f34cc882255ec1b0b2a2b6846041168f0091
alessiogentilezza <alessio.gentilezza@gmail.com>
2023-05-10 15:25:54 +0200

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+
+@section('page-title', 'Home page')
+
+@section('content')
+    <h3>Main</h3>
+@endsection
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,5 @@
+<footer>
+    <div class="container">
+        <h3>Header</h3>
+    </div>
+</footer>
